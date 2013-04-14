dfd59fcc0c877a3f9851412fbfe9d23f10d47e9d
Noam Rubin <rubin.m.noam@gmail.com>
2013-04-14 16:12:52 -0400

    added plotting code to main.m, and exp2 and exp3 figs

--- main.m
+++ main.m
@@ -1,2 +1,8 @@
 doplot('Exp1P1','./Figures/','Exp1P1');
 doplot('Exp1P2','./Figures/','Exp1P2');
+doplot('Exp2P1','./Figures/','Exp2P1');
+doplot('Exp2P2','./Figures/','Exp2P2');
+doplot('Exp2P3','./Figures/','Exp2P3');
+doplot('Exp3P1','./Figures/','Exp3P1');
+doplot('Exp3P2','./Figures/','Exp1P2');
+
